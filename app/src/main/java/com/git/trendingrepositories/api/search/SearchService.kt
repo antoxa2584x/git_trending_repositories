88f435e8702c86c094bde084af88d4362e252940
AntonTribu <anton@we-tribu.com>
2023-10-09 21:01:50 +0300

    Provided search service and network module

---
+++ app/src/main/java/com/git/trendingrepositories/api/search/SearchService.kt
@@ -0,0 +1,16 @@
+package com.git.trendingrepositories.api.search
+
+import com.git.trendingrepositories.api.search.model.ApiRepositorySearchModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SearchService {
+    @GET("search/repositories")
+    suspend fun getSearchRepositories(
+        @Query("q") search: String,
+        @Query("page") page: Int = 0,
+        @Query("sort") sort: String,
+        @Query("order") order: String,
+    ): Response<ApiRepositorySearchModel>
+}
---
+++ app/src/main/java/com/git/trendingrepositories/di/NetworkModule.kt
@@ -0,0 +1,40 @@
+package com.git.trendingrepositories.di
+
+import com.git.trendingrepositories.api.search.SearchService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+    companion object {
+        private const val BASE_URL = "https://api.github.com"
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSearchService(retrofit: Retrofit): SearchService {
+        return retrofit.create(SearchService::class.java)
+    }
+}
